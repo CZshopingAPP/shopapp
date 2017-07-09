78ed6049f892770921f7499ecd157fad935b790b
Yyyyyyyyujie <1060173122@qq.com>
2017-07-09 19:05:34 +0800

    yy

--- yj-login.php
+++ yj-login.php
@@ -10,6 +10,19 @@
 	<script src="js/jQuery.js"></script>
 	<script src="js/jquery.validate.js"></script>
 		<script src="js/messages_zh.js"></script>
+	<style>
+		.loginn{
+			background-image: url("img/yj-login-login.png");
+			background-size: 100%;
+			background-position: center;
+			border: none;
+			border-radius: 0.4rem;
+			margin-top: 0.6rem;
+		}
+		.pass{
+			margin-top: -2.45rem;
+		}
+	</style>
 </head>
 <body>
 	<div class="box">
@@ -38,7 +51,7 @@
                     </div>
                 </div>
             </div>
-        </form>
+
 		<div class="tiao"></div>
 		<div class="user1"></div>
 		<div class="pass"></div>
@@ -46,16 +59,9 @@
 			<div class="message1"></div>
 			<span>您填写的用户名有误</span>
 		</div>
-        
-        <form action="Checklogin.php">
-            <div class="loginn">
-                <span></span>
-                <span>
-				<p>登录</p>
-				<p>DENGLUHERE</p>
-			</span>
-                <span></span>
-            </div>
+
+			<input type="button" class="loginn">
+
         </form>
 		
         
@@ -68,19 +74,40 @@
 	</div>
 </body>
 <script type="text/javascript">
+
+	$(".pass").click(function () {
+		if($(".input").val()==""){
+			$(".pass").disabled();
+		}
+	})
 	$(".pass").click(function(){
 		$(".usertitle").css("background-image","url('img/yj-login-userpass.png')");
 		$(".input").hide();
 		$(".password").show();
 		$(this).css("background-image","url('img/yj-login-pass1.png')");
+		$(".xuan1").css("width","0.08rem");
+		$(".xuan2").css("width","0.19rem");
 	})
 	$(".user1").click(function(){
 		$(".usertitle").css("background-image","url('img/yj-login-username.png')");
 		$(".input").show();
 		$(".password").hide();
+		$(".xuan2").css("width","0.08rem");
+		$(".xuan1").css("width","0.19rem");
 	})
-	$(".loginn").click(function () {
-        
-    })
+	/*if($(".input").val()==""){
+		if($(".password").val()==""){
+			$(".loginn").disable();
+		}
+	}*/
+	$(".password").blur(function () {
+		if($(".password").val()!=""){
+			$(".loginn").css("background-image","url('img/yj-login-login1.png')")
+		}
+	})
+
+
+
+
 </script>
 </html>
