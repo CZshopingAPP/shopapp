4364c3191fe42d6bfc1bd4d85eb6eae6e8eb6c09
Yyyyyyyyujie <1060173122@qq.com>
2017-07-09 20:38:00 +0800

    yy

--- yj-reg.php
+++ yj-reg.php
@@ -7,6 +7,26 @@
 	<link rel="stylesheet" href="css/base.css">
 	<link rel="stylesheet" href="css/yj-reg.css">
 	<script src="js/rem.js"></script>
+	<script src="js/jQuery.js"></script>
+	<script src="js/jquery.validate.js"></script>
+	<script src="js/messages_zh.js"></script>
+
+	<style>
+		.loginn{
+			background-image: url("img/yj-reg-reg1.png");
+			background-size: 100%;
+			background-position: center;
+			border: none;
+			border-radius: 0.4rem;
+			/*margin-top: 0.6rem;*/
+			position: fixed;
+			bottom: 1.5rem;
+		}
+       .message span{
+		   margin-left: 0.6rem;
+		   margin-top: -0.45rem;
+	   }
+	</style>
 </head>
 <body>
 	<div class="box">
@@ -20,7 +40,8 @@
 				<span></span>
 				<span></span>
 			</div>
-			<input type="text" class="user" placeholder="请输入手机号码或邮箱" name="mname">
+			<form action="" class="myform">
+			<input type="text" class="user phone" placeholder="请输入手机号码或邮箱" name="mname">
 			<input type="text" class="code" placeholder="请输入验证码">
 			<input type="password" class="pass" placeholder="请输入密码" name="mpass">
 			<input type="password" class="pass1" placeholder="请再次输入密码" name="mpass1">
@@ -28,16 +49,58 @@
 		</div>
 		<div class="message">
 			<div class="message1"></div>
-			<span>您填写的用户名有误</span>
-		</div>
-		<div class="loginn">
-			<span></span>
-			<span>
-				<p>注册</p>
-				<p>REGIT</p>
-			</span>
 			<span></span>
 		</div>
+		<input type="button" class="loginn">
+
+		</form>
 	</div>
 </body>
+<script>
+	$("input").blur(function () {
+		if($(".user").val()!=""){
+			if($(".code").val()!=""){
+				if($(".pass").val()!=""){
+					if($(".pass1").val()!=""){
+						$(".loginn").css("background-image","url('img/yj-reg-reg.png')")
+					}
+				}
+
+			}
+		}
+	})
+		jQuery.validator.addMethod("isMobile", function(value, element) {
+		var length = value.length;
+		var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+		return this.optional(element) || (length == 11 && mobile.test(value));
+	}, "请正确填写您的手机号码");
+
+	$(".myform").validate({
+		errorLabelContainer:".message span",
+		rules: {
+			/*user: {
+				required: true,
+				email: true,
+
+			},*/
+			mname: {
+				required: true,
+				minlength: 11,
+				// 自定义方法：校验手机号在数据库中是否存在
+				// checkPhoneExist : true,
+				isMobile: true,
+				email:true,
+			},
+		},
+		messages: {
+			mname: {
+				required: "请输入手机号",
+				minlength: "确认手机不能小于11个字符",
+				isMobile: "请正确填写您的手机号码"
+			},
+		}
+	})
+
+
+</script>
 </html>
